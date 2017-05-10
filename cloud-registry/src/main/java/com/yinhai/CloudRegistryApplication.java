b83f876d4b00380bff727e02b6180f6ba3122f67
18181956331 <18181956331@163.com>
2017-05-10 16:00:50 +0800

    registry 注释修改

--- cloud-registry/src/main/java/com/yinhai/CloudRegistryApplication.java
+++ cloud-registry/src/main/java/com/yinhai/CloudRegistryApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-// 启动一个服务注册中心
+// 启动一个EurekaServer
 @EnableEurekaServer
 @SpringBootApplication
 public class CloudRegistryApplication {
