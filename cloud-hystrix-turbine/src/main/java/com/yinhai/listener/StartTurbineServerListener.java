5f47c64d5abb39c4317fcce71f39d97ebe459d92
18181956331 <18181956331@163.com>
2017-05-12 17:28:15 +0800

    提交 hystrix turbine

---
+++ cloud-hystrix-turbine/src/main/java/com/yinhai/listener/StartTurbineServerListener.java
@@ -0,0 +1,34 @@
+package com.yinhai.listener;
+
+import com.netflix.turbine.init.TurbineInit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Desc <p></p>
+ * Package com.yinhai.listener
+ * Created by 刘惠涛 on 2017/5/12.
+ */
+@WebListener
+public class StartTurbineServerListener implements ServletContextListener {
+    private static final Logger logger = LoggerFactory.getLogger(StartTurbineServerListener.class);
+
+    public StartTurbineServerListener(){
+
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        logger.info("Initing Turbine server");
+        TurbineInit.init();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        logger.info("Stopping Turbine server");
+    }
+}
