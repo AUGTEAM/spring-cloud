1b5add95e5a10a2e5eebf90c74c6a34ec538e0a8
18181956331 <18181956331@163.com>
2017-05-17 15:46:43 +0800

    修改

--- cloud-config-client/src/test/java/com/yinhai/CloudConfigClientApplicationTests.java
+++ cloud-config-client/src/test/java/com/yinhai/CloudConfigClientApplicationTests.java
@@ -4,13 +4,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class CloudConfigClientApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		RestTemplate restTemplate = new RestTemplate();
+		String object = restTemplate.postForObject("http://localhost:7001/bus/refresh",null,String.class);
+		System.out.println(object);
 	}
 
 }
