344cfa777831aa550b8f553c7b79a1f5a15a8397
刘惠涛 <18181956331@163.com>
2017-06-08 14:12:08 +0800

    spring boot rabbitmq 配置

---
+++ boot-demo/src/main/java/com/yinhai/mq/Send.java
@@ -0,0 +1,39 @@
+package com.yinhai.mq;
+
+import com.yinhai.config.AmqpConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * Desc <p></p>
+ * Package com.yinhai.mq
+ * Created by liuht on 2017/6/8.
+ */
+@Component
+public class Send implements RabbitTemplate.ConfirmCallback{
+    private final RabbitTemplate rabbitTemplate;
+
+    //@Autowired
+    public Send(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMsg(String content) {
+        CorrelationData correlationId = new CorrelationData(UUID.randomUUID().toString());
+        rabbitTemplate.convertAndSend(AmqpConfig.EXCHANGE, AmqpConfig.ROUTINGKEY, content, correlationId);
+    }
+
+    @Override
+    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+        System.out.println(" 回调id:" + correlationData);
+        if (ack) {
+            System.out.println("消息成功消费");
+        } else {
+            System.out.println("消息消费失败:" + cause);
+        }
+    }
+}
